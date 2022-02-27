0ec8b8ef5ca4a4fb4101ad9930f86eba40621667
Sivagurunathan Sasthan <s.sasthan@hotmail.com>
2022-02-27 17:06:06 +0800

    Added card view animation

--- components/SingleCard.jsx
+++ components/SingleCard.jsx
@@ -1,17 +1,16 @@
-import { React } from 'react';
+import { React, useRef, useState } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   TouchableHighlight,
+  Animated,
 } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderColor: '#003B73',
-    borderRadius: 8,
-    borderWidth: 4,
     margin: 8,
     height: 100,
   },
@@ -21,13 +20,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#0074B7',
+    borderRadius: 8,
+    borderWidth: 4,
   },
 
   backView: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#60A3D9',
+    borderRadius: 8,
+    borderWidth: 4,
   },
 
   cardText: {
@@ -35,26 +39,77 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: '#BFD7ED',
   },
+
+  backAnimatedView: {
+    position: 'absolute',
+    top: 0,
+  },
 });
 
 // eslint-disable-next-line react/prop-types
 export default function SingleCard({ card, handleChoice, flipped }) {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const [aValue, setAValue] = useState(0);
+
+  const doAFlip = () => {
+    if (aValue >= 90) {
+      setAValue(0);
+      Animated.spring(animatedValue, {
+        toValue: 0,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setAValue(180);
+      Animated.spring(animatedValue, {
+        toValue: 180,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
+  const frontInterpolate = animatedValue.interpolate({
+    inputRange: [0, 180],
+    outputRange: ['0deg', '180deg'],
+  });
+
+  const backInterpolate = animatedValue.interpolate({
+    inputRange: [0, 180],
+    outputRange: ['180deg', '360deg'],
+  });
+
+  const frontAnimatedStyle = {
+    transform: [{ rotateY: frontInterpolate }],
+  };
+
+  const backAnimatedStyle = {
+    transform: [{ rotateY: backInterpolate }],
+  };
+
   const handleClick = () => {
     handleChoice(card);
+    doAFlip();
   };
 
   const frontView = (
     <TouchableHighlight onPress={handleClick} style={styles.frontView}>
-      <View>
-        <Text style={styles.cardText}>?</Text>
-      </View>
+      <Animated.View style={frontAnimatedStyle}>
+        <View>
+          <Text style={styles.cardText}>?</Text>
+        </View>
+      </Animated.View>
     </TouchableHighlight>
   );
 
   const backView = (
-    <View style={styles.backView}>
-      <Text style={styles.cardText}>{card.number}</Text>
-    </View>
+    <Animated.View style={backAnimatedStyle}>
+      <View style={styles.backView}>
+        <Text style={styles.cardText}>{card.number}</Text>
+      </View>
+    </Animated.View>
   );
 
   return <View style={styles.container}>{flipped ? backView : frontView}</View>;
